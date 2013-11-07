e6570f74ad8d28f9a8ebe085347faac186501244
Paul Libbrecht <paul@hoplahup.net>
2013-11-07 12:54:02 +0100

    mit Modul.

--- src/main/java/phWGinfo/fundIdeen_02/Main.java
+++ src/main/java/phWGinfo/fundIdeen_02/Main.java
@@ -1,11 +1,21 @@
 package phWGinfo.fundIdeen_02;
 
+import java.io.File;
+
 /**
  *
  */
 public class Main {
 
     public static void main(String[] args) throws Throwable {
+        File file = new File("LICENSE");
+        System.out.println(file.exists());
+        file=file.getAbsoluteFile();
+
+        File file2 = file.getParentFile();
+        System.out.println(file2.getName());
+        System.out.println(file2.equals(file));
+
     }
 
 }
