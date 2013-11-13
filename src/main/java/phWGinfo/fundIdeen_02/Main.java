7ce4ccd80e2ee56d65cee4c8eece605758bceadc
regina <regina.puls@t-online.de>
2013-11-13 20:52:39 +0100

    mit Modul.

--- src/main/java/phWGinfo/fundIdeen_02/Main.java
+++ src/main/java/phWGinfo/fundIdeen_02/Main.java
@@ -8,6 +8,14 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws Throwable {
+
+        int x;
+        x=999;
+        x=x+1;
+        System.out.println(x);
+
+
+
         File file = new File("LICENSE");
         System.out.println(file.exists());
         file=file.getAbsoluteFile();
